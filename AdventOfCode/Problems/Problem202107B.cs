8170a7ee50c8d4042f38bc81363ac25c57882ab4
Andrew J Said <andrewjsaid@gmail.com>
2021-12-08 18:49:54 +0000

    Problems 202107A, 202107B

---
+++ AdventOfCode/Problems/Problem202107B.cs
@@ -0,0 +1,55 @@
+﻿namespace AdventOfCode.Problems
+{
+    internal class Problem202107B : IProblem
+    {
+        public string Solve(ProblemInput input)
+        {
+            var hPositions = input.GetNumbersCsv();
+
+            var length = hPositions.Max() - hPositions.Min() + 1;
+            var cumulatives = LoadCumulativeNumbers(length);
+
+            var mean = (int)hPositions.Average();
+            var meanCost = CalculateFuelCost(hPositions, mean, cumulatives);
+            var meanUpperCost = CalculateFuelCost(hPositions, mean + 1, cumulatives);
+
+            var direction = meanUpperCost < meanCost ? 1 : -1;
+
+            var current = mean;
+            var currentCost = meanCost;
+            while (true)
+            {
+                var next = current + direction;
+                var nextCost = CalculateFuelCost(hPositions, next, cumulatives);
+                if (nextCost > currentCost)
+                {
+                    return currentCost.ToString();
+                }
+                current = next;
+                currentCost = nextCost;
+            }
+        }
+
+        private static int CalculateFuelCost(int[] hPositions, int target, int[] cumulatives)
+        {
+            var result = 0;
+            foreach (var h in hPositions)
+            {
+                result += cumulatives[Math.Abs(target - h)];
+            }
+            return result;
+        }
+
+        private static int[] LoadCumulativeNumbers(int count)
+        {
+            var accumulator = 0;
+            var results = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                accumulator += i;
+                results[i] = accumulator;
+            }
+            return results;
+        }
+    }
+}
