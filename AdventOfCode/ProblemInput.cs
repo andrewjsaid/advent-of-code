05583d88dc294ae4b973b0f6398b29310bdf68ec
Andrew J Said <andrewjsaid@gmail.com>
2021-12-08 00:57:38 +0000

    Problems 202106A and 202106B

--- AdventOfCode/ProblemInput.cs
+++ AdventOfCode/ProblemInput.cs
@@ -14,5 +14,7 @@
         public string[] GetStrings() => _input;
 
         public int[] GetNumbers() => Array.ConvertAll(_input, int.Parse);
+
+        public int[] GetNumbersCsv() => Array.ConvertAll(_input[0].Split(','), int.Parse);
     }
 }
