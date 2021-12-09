289cefcb0d779ebfd0be19b60f63dd9b9b190803
Andrew J Said <andrewjsaid@gmail.com>
2021-12-09 23:26:34 +0000

    Problems 201502A, 201502B

---
+++ AdventOfCode/Problems/Y2015/Problem201502A.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Problems.Y2015;
+
+internal class Problem201502A : IProblem
+{
+    public string Solve(ProblemInput input)
+    {
+        var regex = new Regex("^(?<l>\\d+)x(?<w>\\d+)x(?<h>\\d+)$");
+        var sides = input.GetStrings().Select(s =>
+        {
+            var match = regex.Match(s);
+            var l = int.Parse(match.Groups["l"].Value);
+            var w = int.Parse(match.Groups["w"].Value);
+            var h = int.Parse(match.Groups["h"].Value);
+            return new { Length = l, Width = w, Height = h };
+        });
+
+        return sides.Sum(s =>
+        {
+            var lw = s.Length * s.Width;
+            var wh = s.Width * s.Height;
+            var lh = s.Length * s.Height;
+            return 2 * (lw + wh + lh) + Math.Min(lh, Math.Min(wh, lw));
+        }).ToString();
+    }
+}
---
+++ AdventOfCode/Problems/Y2015/Problem201502B.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Problems.Y2015;
+
+internal class Problem201502B : IProblem
+{
+    public string Solve(ProblemInput input)
+    {
+        var regex = new Regex("^(?<l>\\d+)x(?<w>\\d+)x(?<h>\\d+)$");
+        var sides = input.GetStrings().Select(s =>
+        {
+            var match = regex.Match(s);
+            var l = int.Parse(match.Groups["l"].Value);
+            var w = int.Parse(match.Groups["w"].Value);
+            var h = int.Parse(match.Groups["h"].Value);
+            return new { Length = l, Width = w, Height = h };
+        });
+
+        return sides.Sum(s =>
+        {
+            var p1 = 2 * (s.Length + s.Width);
+            var p2 = 2 * (s.Length + s.Height);
+            var p3 = 2 * (s.Width + s.Height);
+            var p = Math.Min(p1, Math.Min(p2, p3));
+            var bow = s.Length * s.Width * s.Height;
+            return p + bow;
+        }).ToString();
+    }
+}
