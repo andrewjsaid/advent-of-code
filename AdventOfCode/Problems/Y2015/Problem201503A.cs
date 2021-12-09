ddb7d1666243caf0ce40a4465c31b5e6bdc1356c
Andrew J Said <andrewjsaid@gmail.com>
2021-12-09 23:36:13 +0000

    Problems 201503A, 201503B

---
+++ AdventOfCode/Problems/Y2015/Problem201503A.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode.Problems.Y2015;
+
+internal class Problem201503A : IProblem
+{
+    public string Solve(ProblemInput input)
+    {
+        var directions = input.GetString();
+        return new HashSet<(int x, int y)>(Run(directions)).Count.ToString();
+    }
+
+    private static IEnumerable<(int x, int y)> Run(IEnumerable<char> directions)
+    {
+        int x = 0;
+        int y = 0;
+        yield return (x, y);
+
+        foreach (var direction in directions)
+        {
+            if (direction == '<')
+            {
+                x--;
+            }
+            else if (direction == '>')
+            {
+                x++;
+            }
+            else if (direction == '^')
+            {
+                y++;
+            }
+            else if (direction == 'v')
+            {
+                y--;
+            }
+            yield return (x, y);
+        }
+    }
+}
---
+++ AdventOfCode/Problems/Y2015/Problem201503B.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode.Problems.Y2015;
+
+internal class Problem201503B : IProblem
+{
+    public string Solve(ProblemInput input)
+    {
+        var directions = input.GetString();
+
+        var d1 = new List<char>();
+        var d2 = new List<char>();
+
+        foreach (var direction in directions)
+        {
+            d1.Add(direction);
+            var d3 = d1;
+            d1 = d2;
+            d2 = d3;
+        }
+
+        var hs = new HashSet<(int x, int y)>();
+        hs.UnionWith(Run(d1));
+        hs.UnionWith(Run(d2));
+        return hs.Count.ToString();
+    }
+
+    private static IEnumerable<(int x, int y)> Run(IEnumerable<char> directions)
+    {
+        int x = 0;
+        int y = 0;
+        yield return (x, y);
+
+        foreach (var direction in directions)
+        {
+            if (direction == '<')
+            {
+                x--;
+            }
+            else if (direction == '>')
+            {
+                x++;
+            }
+            else if (direction == '^')
+            {
+                y++;
+            }
+            else if (direction == 'v')
+            {
+                y--;
+            }
+            yield return (x, y);
+        }
+    }
+}
