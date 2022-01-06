b384a17c143fba472d1a57399aa06ce0aebf6299
Andrew J Said <andrewjsaid@gmail.com>
2022-01-06 17:48:58 +0000

    Problems 202114A, 202114B

---
+++ AdventOfCode/Problems/Y2021/Problem202114A.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace AdventOfCode.Problems.Y2021;
+
+internal class Problem202114A : IProblem
+{
+    public string Solve(ProblemInput input)
+    {
+        var lines = input.GetStrings();
+        var template = lines[0];
+
+        KeyValuePair<string, string> ParseLine(string line) => new(line[..2], line[^1..]);
+        var formulae = new Dictionary<string, string>(lines.Skip(2).Select(ParseLine));
+
+        var sb = new StringBuilder();
+
+        var step = 1;
+        do
+        {
+            sb.Clear();
+
+            for (var i = 0; i < template.Length - 1; i++)
+            {
+                sb.Append(template[i]);
+                var pair = template[i..(i + 2)];
+                if (formulae.TryGetValue(pair, out var insert))
+                {
+                    sb.Append(insert);
+                }
+            }
+            sb.Append(template[^1]);
+
+            template = sb.ToString();
+
+        } while (++step <= 10);
+
+        var elements = template
+            .GroupBy(x => x)
+            .Select(g => new { Element = g.Key, Count = g.Count() })
+            .ToArray();
+
+        var min = elements.Min(c => c.Count);
+        var max = elements.Max(c => c.Count);
+
+        return (max - min).ToString();
+    }
+}
---
+++ AdventOfCode/Problems/Y2021/Problem202114B.cs
@@ -0,0 +1,75 @@
+﻿namespace AdventOfCode.Problems.Y2021;
+
+internal class Problem202114B : IProblem
+{
+    private const int NumSteps = 40;
+
+    private readonly Dictionary<(char left, char right, int depth), Dictionary<char, long>> _cache = new();
+    private readonly Dictionary<(char left, char right), char> _formulae = new();
+
+    public string Solve(ProblemInput input)
+    {
+        var lines = input.GetStrings();
+        var template = lines[0];
+
+        foreach (var line in lines.Skip(2))
+        {
+            _formulae.Add((line[0], line[1]), line[^1]);
+        }
+
+        var grouped = new Dictionary<char, long>();
+        for (var i = 0; i < template.Length -1; i++)
+        {
+            var inner = Run(1, template[i], template[i + 1]);
+            Merge(inner, grouped);
+        }
+
+        foreach (var c in template)
+        {
+            grouped.TryGetValue(c, out var value);
+            grouped[c] = value + 1;
+        }
+
+        return (grouped.Values.Max() - grouped.Values.Min()).ToString();
+    }
+
+    private Dictionary<char, long> Run(int step, char left, char right)
+    {
+        if (step > NumSteps)
+        {
+            return new();
+        }
+
+        var distance = NumSteps - step;
+        if (!_cache.TryGetValue((left, right, distance), out var result))
+        {
+            result = new();
+
+            if (_formulae.TryGetValue((left, right), out var insert))
+            {
+                var leftPair = Run(step + 1, left, insert);
+                var rightPair = Run(step + 1, insert, right);
+
+                result[insert] = 1;
+
+                Merge(leftPair, result);
+                Merge(rightPair, result);
+            }
+
+            _cache.Add((left, right, distance), result);
+        }
+
+        return result;
+    }
+
+    private static void Merge(Dictionary<char, long> source, Dictionary<char, long> target)
+    {
+        foreach (var (c, increment) in source)
+        {
+            target.TryGetValue(c, out var existing);
+            target[c] = existing + increment;
+        }
+    }
+
+
+}
